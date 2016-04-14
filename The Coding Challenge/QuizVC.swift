55e57f84e17e9c24ed1f9affb7f1b4a9ba991bf8
Jarle <jarle.matland@gmail.com>
2016-04-14 12:59:00 +0200

    addd quiz icons and QuizVC

---
+++ The Coding Challenge/QuizVC.swift
@@ -0,0 +1,16 @@
+//
+//  QuizVC.swift
+//  The Coding Challenge
+//
+//  Created by Jarle Matland on 14.04.2016.
+//  Copyright © 2016 Donkey Monkey. All rights reserved.
+//
+
+import UIKit
+
+class QuizVC: UIViewController {
+    
+    
+    
+    
+}
